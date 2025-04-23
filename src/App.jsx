ec2583663afbda3379de283e1daee4a42ae40373
Demar English <demarenglish@Mac.attlocal.net>
2025-04-23 15:39:26 -0400

    Importing components to top level component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Nav />
     </>
   )
 }
