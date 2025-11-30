dd7895b885c2c05cafabad1ee92ade74ae1ed549
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-30 13:00:03 -0500

    Adding components to the layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Nav from "./components/Nav"
+import Homepage from "./components/Homepage"
+import Specials from "./components/Specials"
+import Testimonial from "./components/Testimonial"
 
 function App() {
 
   return (
     <>
       <Nav />
+      <Homepage />
+      <Specials />
+      <Testimonial />
     </>
   )
 }
