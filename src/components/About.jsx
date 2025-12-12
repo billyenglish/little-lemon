f53fec3bedad1ace96b2e9016c29014787b4603b
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-12-12 18:33:52 -0500

    About me section.

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+const About = () => {
+
+    return (
+        <section id="about_experience">
+            <article id="about_intro">
+                <h2 className="section_title">
+                    About Little Lemon
+                </h2>
+
+                <p>
+
+                </p>
+            </article>
+            <article id="about_images">
+
+            </article>
+        </section>
+
+    )
+}
+
+export default About;
