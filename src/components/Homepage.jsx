3feddecefa2e1465fd0ae9f935f81821631d6134
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-12-13 14:40:22 -0500

    Updated paragraph id to className.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,3 +1,4 @@
+import "../media_queries/homepage_query.css"
 import FinDining from "../assets/FineDining.png"
 
 const Homepage = () => {
@@ -9,18 +10,18 @@ const Homepage = () => {
                     Welcome to Little Lemon
                 </h1>
 
-                <p id="article_paragraph">
+                <p className="article_paragraph">
                     A cozy neighborhood bistro serving fresh, Mediterranean-inspired dishes made from locally sourced ingredients.
                     Join us for vibrant flavors, warm hospitality, and a relaxed atmosphere—perfect for weeknight dinners, weekend
                     brunch, or a quick bite with friends.
                 </p>
 
-                <p id="article_paragraph">
+                <p className="article_paragraph">
                     Join us for vibrant flavors, warm hospitality, and a relaxed atmosphere—perfect for weeknight dinners, weekend
                     brunch, or a quick bite with friends.
                 </p>
 
-                <div>
+                <div className="reserve_table_container">
                     <button className="reserve_a_table">
                         Reserve A Table
                     </button>
@@ -28,6 +29,7 @@ const Homepage = () => {
             </article>
             <article id="welcome_images">
                 <img
+                    className="welcome_image"
                     src={FinDining}
                     height={450}
                     width="auto"
